4d8b36f4efe2b80776fa720f5704e51af7ab8f53
Akalugo Daniel <daniel58@gmail.com>
2022-09-09 11:12:44 +0100

    Added git hooks, eslint, prettier and rubocop

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,5 +1,6 @@
+# frozen_string_literal: true
+
 class Task < ApplicationRecord
-    MAX_TITLE_LENGTH = 125
-    validates :title, presence: true, length: { maximum: MAX_TITLE_LENGTH }
-  end
-  
+  MAX_TITLE_LENGTH = 125
+  validates :title, presence: true, length: { maximum: MAX_TITLE_LENGTH }
+end
