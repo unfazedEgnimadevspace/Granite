1119576e162ef30e9a1d7c4e0200e04a4248352b
Akalugo Daniel <daniel58@gmail.com>
2022-09-14 21:17:16 +0100

    addedd assiociations between user and task

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class Task < ApplicationRecord
+  belongs_to :assigned_user, class_name: "User", foreign_key: "assigned_user_id"
   MAX_TITLE_LENGTH = 125
   validates :title, presence: true, length: { maximum: MAX_TITLE_LENGTH }
   validates :slug, uniqueness: true
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
+  has_many :assigned_task, class_name: "Task", foreign_key: :assigned_user_id
   MAXIMUM_NAME_LENGTH = 255
   validates :name, presence: true, length: { maximum: MAXIMUM_NAME_LENGTH }
 end
